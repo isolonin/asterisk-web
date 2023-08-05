8ec3c940ae367df5673c7bad743f2780bb762a36
Alex Notsky <anotsky12@gmail.com>
2023-08-06 00:49:09 +0300

    pagination with one page

--- src/main/java/i/solonin/asteriskweb/AsteriskWebApplication.java
+++ src/main/java/i/solonin/asteriskweb/AsteriskWebApplication.java
@@ -2,6 +2,7 @@ package i.solonin.asteriskweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class AsteriskWebApplication {
